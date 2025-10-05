6269882367475f5fd86b36ba24737dbaa8eeb6ee
Kalyan Jyoti Borah <kalyanborah456@gmail.com>
2025-10-05 23:21:31 +0530

    Update server deployment config and adjust .gitignore

--- server/Server.js
+++ server/Server.js
@@ -39,14 +39,12 @@ app.use("/api/user", userRoutes);
 app.use("/api/chat", chatRoutes);
 app.use("/api/message", messageRoutes);
 
-//Deployment
-const __dirname1 = path.resolve();
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname1, "../client_app/dist")));
+if (process.env.NODE_ENV === "development") {
+  app.use(express.static(path.join(__dirname, "../client_app/dist")));
 
   app.get("*", (request, response) => {
     response.sendFile(
-      path.resolve(__dirname1, "../client_app", "dist", "index.html"),
+      path.resolve(__dirname, "../client_app", "dist", "index.html"),
     );
   });
 } else {
